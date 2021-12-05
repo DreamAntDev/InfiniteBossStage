25cf8db0af22854deaa202bf1b7c2a317efeeabd
KwakDongKyu <dongkyu0608@gmail.com>
2021-12-05 22:03:34 +0900

    TMP 추가

--- Assets/Kwak/Controller/OnScreenJoystick.cs
+++ Assets/Kwak/Controller/OnScreenJoystick.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.OnScreen;
 using UnityEngine.InputSystem.Layouts;
-
+using System.Linq;
 
 namespace Common.Controller
 {
@@ -17,10 +17,37 @@ namespace Common.Controller
         private RectTransform rectTransform;
         private void Awake()
         {
+            // Common하게 쓰려면 InputActionAsset을 링크해서 사용하도록 수정해야 한다.
             PlayerInputActions playerInputActions = new PlayerInputActions();
-            playerInputActions.Player.Enable();
-            playerInputActions.Player.Move.performed += SetPosition;
-            playerInputActions.Player.Move.canceled += ResetPosition;
+            InputActionMap inputActionMap = null;
+            InputAction inputAction = null;
+
+            foreach(var actionMap in playerInputActions.asset.actionMaps)
+            {
+                var actions = actionMap.actions;
+                foreach(var action in actions)
+                {
+                    int findIdx = action.bindings.IndexOf(o => o.path.Equals(this.controlPath));
+                    if(findIdx >= 0)
+                    {
+                        inputAction = action;
+                        inputActionMap = actionMap;
+                        break;
+                    }
+                }
+                if(inputAction != null && inputActionMap != null)
+                {
+                    break;
+                }
+            }
+
+            inputActionMap.Enable();
+            inputAction.performed += SetPosition;
+            inputAction.canceled += ResetPosition;
+
+            //playerInputActions.Player.Enable();
+            //playerInputActions.Player.Move.performed += SetPosition;
+            //playerInputActions.Player.Move.canceled += ResetPosition;
 
             this.rectTransform = GetComponent<RectTransform>();
         }
@@ -46,6 +73,8 @@ namespace Common.Controller
         public void OnPointerDown(PointerEventData eventData)
         {
             // eventData.position 은 좌하단 기준 좌표, rectTransform도 앵커를 좌하단으로 잡고 피봇을 중심으로 맞추면 동일 좌표계로 사용 가능하다
+            // Common하게 앵커에 맞도록 UI상 좌표와 스크린좌표를 맞추는 작업이 필요하다
+            // eventData로 넘어오는건 스크린 좌표, 앵커의 위치에 따라 this의 중심 위치가 달라진다.
             var dirVec = eventData.position - this.rectTransform.anchoredPosition;
             
             if (dirVec.magnitude > range)
