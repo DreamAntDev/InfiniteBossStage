3f773af6058e4f5c6ebad5d45fe0f7ab8c23c318
hojin <jhj377@gmail.com>
2022-08-31 15:04:36 +0900

    렐릭 적용시 STATS에 반영되도록

--- Assets/Script/UI/LobbyPage/MainPage.cs
+++ Assets/Script/UI/LobbyPage/MainPage.cs
@@ -22,6 +22,8 @@ namespace UI.Lobby
         public Data.Character.CharacterStatus statusData;
         Character.Status status;
 
+        private const int RELIC_CNT_LIMIT = 3;
+
         public override string GetTitle()
         {
             return "Main";
@@ -74,20 +76,28 @@ namespace UI.Lobby
 
         private void PlayerStatus()
         {
-            Text_HP.text =  this.status.MaxHP.ToString();
-            Text_Stamina.text = this.status.MaxStamina.ToString();
-            Text_Attack.text = this.status.MaxAttack.ToString();
-            Text_Move.text = this.status.MaxMove.ToString();
-            
+
+            Text_HP.text = this.status.MaxHP.ToString() + ((this.status.CurrentHP > this.status.MaxHP) ?  " +"+(this.status.CurrentHP - this.status.MaxHP).ToString() : "");
+            Text_Stamina.text = this.status.MaxStamina.ToString() + ((this.status.CurrentStamina > this.status.MaxStamina) ?  " +"+(this.status.CurrentStamina - this.status.MaxStamina).ToString() : "");
+            Text_Attack.text = this.status.MaxAttack.ToString() + ((this.status.CurrentAttack > this.status.MaxAttack) ?  " +"+(this.status.CurrentAttack - this.status.MaxAttack).ToString() : "");
+            Text_Move.text = this.status.MaxMove.ToString() + ((this.status.CurrentMove > this.status.MaxMove) ?  " +"+(this.status.CurrentMove - this.status.MaxMove).ToString() : "");
+
             List<Relic> activeRelics = RelicManager.Instance.ActivePlayerRelic();
             Debug.Log("PlayerStatus ActiveRelic:" + activeRelics.Count);
-            for(int i = 0; i < activeRelics.Count; i++)
+            // var image_Relic_default = relicUI.transform.Find("relic_icon_default").GetComponent<Image>();
+            for(int i = 0; i < RELIC_CNT_LIMIT; i++)
             {
                 var relicUI = GameObject.Find("Button_Relic_" + i.ToString());
                 var image_Relic = relicUI.transform.Find("Icon_Sub").GetComponent<Image>();
                 var text_Relic = relicUI.GetComponentInChildren<TextMeshProUGUI>();
-                image_Relic.sprite = activeRelics[i].Sprite;
-                text_Relic.text = activeRelics[i].ToString();
+                if ((activeRelics.Count - i) > 0) {
+                    image_Relic.enabled = true;
+                    image_Relic.sprite = activeRelics[i].Sprite;
+                    text_Relic.text = activeRelics[i].ToString();
+                } else {
+                    image_Relic.enabled = false;
+                    text_Relic.text = "";
+                }
             }
         }
     }
